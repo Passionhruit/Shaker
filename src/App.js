ba13c3130d0af3096de2da4da4dcdab84c70a1fa
[Mia Lee] <korea_stan@naver.com>
2023-07-13 14:52:30 +0900

    add globalstyle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Router from "./shared/Router";
 import { Reset } from "styled-reset";
 import { useEffect } from "react";
 import app from "./service/firebase";
+import GlobalStyle from "./GlobalStyle";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ function App() {
   // }, []);
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       <Reset />
       <Router />
     </QueryClientProvider>
